01ba3d676199dc58a947dff8cd79fba35b9d5de8
Tindra Admin <motechgroupkenya@gmail.com>
2026-07-19 13:13:46 +0300

    Bind global SMTP configurations from settings table dynamically at runtime

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        try {
+            if (\Illuminate\Support\Facades\Schema::hasTable('settings')) {
+                $mailHost = \App\Models\Setting::get('mail_host');
+                if ($mailHost) {
+                    config([
+                        'mail.default' => 'smtp',
+                        'mail.mailers.smtp.host' => $mailHost,
+                        'mail.mailers.smtp.port' => \App\Models\Setting::get('mail_port', '2525'),
+                        'mail.mailers.smtp.username' => \App\Models\Setting::get('mail_username'),
+                        'mail.mailers.smtp.password' => \App\Models\Setting::get('mail_password'),
+                        'mail.mailers.smtp.encryption' => \App\Models\Setting::get('mail_encryption', 'tls'),
+                        'mail.from.address' => \App\Models\Setting::get('mail_from_address', 'hello@saasninja.top'),
+                        'mail.from.name' => \App\Models\Setting::get('company_name', 'SaaSNinja'),
+                    ]);
+                }
+            }
+        } catch (\Exception $e) {
+            // Silence exceptions during early bootstrap or migration phases
+        }
     }
 }
