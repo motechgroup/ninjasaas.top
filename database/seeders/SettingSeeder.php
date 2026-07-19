086ef7d43727dc7fe4eb144a599f75b4adaebb13
Tindra Admin <motechgroupkenya@gmail.com>
2026-07-19 22:59:56 +0300

    Bind Stripe settings configuration dynamically to environment helper variables to avoid committing API keys

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -13,5 +13,7 @@ class SettingSeeder extends Seeder
         Setting::set('support_email', 'support@saasninja.top', 'string');
         Setting::set('allow_registrations', 'true', 'boolean');
         Setting::set('envato_sandbox_mode', 'true', 'boolean');
+        Setting::set('stripe_public_key', env('STRIPE_PUBLIC_KEY', 'stripe_mock_public_key_fallback'), 'string');
+        Setting::set('stripe_secret_key', env('STRIPE_SECRET_KEY', 'stripe_mock_secret_key_fallback'), 'string');
     }
 }
