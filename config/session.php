f362c53dcb2b685f9b9ba1cf766673c3044eab86
Tindra Admin <motechgroupkenya@gmail.com>
2026-07-21 00:51:59 +0300

    Configure RFC 6265bis __Host- cookie prefix and enforce secure=true for session cookies

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug((string) env('APP_NAME', 'laravel')).'-session'
+        '__Host-saasninja_session'
     ),
 
     /*
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------
